e1345ed6590e3b0f7a25d50bb92757b5f5b1a2cd
matsrietdijk <matsrietdijk@gmail.com>
2013-12-09 10:58:39 +0100

    Fixed default file

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require "csv"
 require_relative "lib/r"
 require_relative "lib/cmd"
 
-file   = CMD.get_input "Path to file: ", type: :file, default: "restaurant.arff"
+file   = CMD.get_input "Path to file: ", type: :file, default: "data/restaurant.arff"
 rows   = CMD.parse file
 column = CMD.get_input "Run ZeroR for column index: ", type: :numeric, length: rows.first.length, default: rows.first.length
 
